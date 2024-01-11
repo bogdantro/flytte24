e5019697baf037e4915d1b08dbaed968c33c6e73
bogdantro <sabertoothtri@gmail.com>
2024-01-11 19:37:18 +0100

    f

---
+++ static/js/home.js
@@ -0,0 +1,89 @@
+
+function verdiStepTwo() {
+    if(document.getElementById('km').value && document.getElementById('reg_nr').value){
+
+        document.getElementById('stepTwo').style.display = 'block';
+    }else{
+        document.getElementById('stepTwo').style.display = 'none';
+    }
+}
+
+setInterval(() => {
+    verdiStepTwo();
+}, 500);
+
+function faqFunc1(){
+    const answer = document.getElementById('answer1');
+    const ask = document.getElementById('ask1');
+    const faqPlus = document.getElementById('faqPlus1');
+    const faqMinus = document.getElementById('faqMinus1');
+    if (window.getComputedStyle(answer,null).getPropertyValue("opacity") == '0'){
+        answer.classList.add('active');
+        ask.style.borderBottom = '1px solid transparent';
+        faqPlus.style.display = 'none';
+        faqMinus.style.display = 'block';
+    }else{
+        answer.classList.remove('active');
+        ask.style.borderBottom = '1px solid lightgray';
+        faqPlus.style.display = 'block';
+        faqMinus.style.display = 'none';
+    }
+}
+
+function faqFunc2(){
+    const answer = document.getElementById('answer2');
+    const ask = document.getElementById('ask2');
+    const faqPlus = document.getElementById('faqPlus2');
+    const faqMinus = document.getElementById('faqMinus2');
+    if (window.getComputedStyle(answer,null).getPropertyValue("opacity") == '0'){
+        answer.classList.add('active');
+        ask.style.borderBottom = '1px solid transparent';
+        faqPlus.style.display = 'none';
+        faqMinus.style.display = 'block';
+    }else{
+        answer.classList.remove('active');
+        ask.style.borderBottom = '1px solid lightgray';
+        faqPlus.style.display = 'block';
+        faqMinus.style.display = 'none';
+    }
+}
+
+function faqFunc3(){
+    const answer = document.getElementById('answer3');
+    const ask = document.getElementById('ask3');
+    const faqPlus = document.getElementById('faqPlus3');
+    const faqMinus = document.getElementById('faqMinus3');
+    if (window.getComputedStyle(answer,null).getPropertyValue("opacity") == '0'){
+        answer.classList.add('active');
+        ask.style.borderBottom = '1px solid transparent';
+        faqPlus.style.display = 'none';
+        faqMinus.style.display = 'block';
+    }else{
+        answer.classList.remove('active');
+        ask.style.borderBottom = '1px solid lightgray';
+        faqPlus.style.display = 'block';
+        faqMinus.style.display = 'none';
+    }
+}
+
+
+function faqFunc4(){
+    const answer = document.getElementById('answer4');
+    const ask = document.getElementById('ask4');
+    const faqPlus = document.getElementById('faqPlus4');
+    const faqMinus = document.getElementById('faqMinus4');
+    if (window.getComputedStyle(answer,null).getPropertyValue("opacity") == '0'){
+        answer.classList.add('active');
+        ask.style.borderBottom = '1px solid transparent';
+        faqPlus.style.display = 'none';
+        faqMinus.style.display = 'block';
+    }else{
+        answer.classList.remove('active');
+        ask.style.borderBottom = '1px solid lightgray';
+        faqPlus.style.display = 'block';
+        faqMinus.style.display = 'none';
+    }
+}
+
+
+

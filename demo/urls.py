79ef49faa1c85a96de03b0a4828579ff391e8aad
bogdantro <sabertoothtri@gmail.com>
2024-01-11 19:11:13 +0100

    d

--- demo/urls.py
+++ demo/urls.py
@@ -8,7 +8,6 @@ from apps.core.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views
-from apps.userprofile.views import * 
 from apps.store.views import *
 from apps.core.sitemaps import *
 from django.contrib.sitemaps.views import sitemap
@@ -57,34 +56,6 @@ urlpatterns = [
     
     path('priser/', price, name='price'),
 
-    # Auth
-    path('logg-inn/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('lag-bruker/', signup, name='signup'),
-    path('logg-ut/', views.LogoutView.as_view(), name='logout'),
-
-    path('min-bruker/', myaccount, name='myaccount'),
-    path('min-bruker/rediger-min-info/', edit_user_info, name='edit_user_info'),
-    path('min-bruker/mine-annonser/', mine_annonser, name='mine_annonser'),
-    path('mine-annonser/egenerkl/<int:car_id>/', mine_annonser_egenerklering, name='mine_annonser_egenerklering'),
-    path('min-bruker/mine-bud/', mine_bud, name='mine_bud'),
-    path('min-bruker/mine-kjøp/', mine_kjøp, name='mine_kjøp'),
-    path('min-bruker/kommende-visninger/', kommende_visninger, name='kommende_visninger'),
-
-    # Buy step one
-    path('step_one/<int:buy_id>/', step_one, name='step_one'),
-    path('step_one_forsikring/<int:buy_id>/', step_one_forsikring, name='step_one_forsikring'),
-    
-    # Buy step two
-    path('step_two/<int:buy_id>/', step_two, name='step_two'),
-    path('step_two_garanti/<int:buy_id>/', step_two_garanti, name='step_two_garanti'),
-
-
-    path('accept_bid/<int:car_id>/', accept_highest_bid, name='accept_highest_bid'),
-    path('decline_bid/<int:car_id>/', decline_highest_bid, name='decline_highest_bid'),
-
-    path('passord-reset/', pass_reset, name='pass_reset'),
-    path('passord-change/', password_change, name='password_change'),
-    path('reset/<uidb64>/<token>', passwordResetConfirm, name='password_reset_confirm'),
 
 
 
